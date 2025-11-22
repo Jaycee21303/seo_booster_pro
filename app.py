46b3cd7a6f0aaf3e7c7a43794b194f2afaa0143e
Jaycee21303 <cantstop216@gmail.com>
2025-11-21 23:02:25 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "SEO Booster is running!"
+def landing():
+    return render_template("landing.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run()
